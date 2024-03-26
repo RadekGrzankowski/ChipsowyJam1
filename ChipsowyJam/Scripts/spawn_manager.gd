31c4aec68f5b3e5da57ac30292a4fa0593cc68a9
Michał Kornaus <michal990909@wp.pl>
2024-03-26 13:48:11 +0100

    Mobs colliders & spawn manager

--- ChipsowyJam/Scripts/spawn_manager.gd
+++ ChipsowyJam/Scripts/spawn_manager.gd
@@ -1,4 +1,12 @@
 extends Node3D
+
+@export var markerTopRed: Marker3D
+@export var markerMidRed: Marker3D
+@export var markerBotRed: Marker3D
+@export var markerTopBlue: Marker3D
+@export var markerMidBlue: Marker3D
+@export var markerBotBlue: Marker3D
+
 @onready var blueDemon = preload("res://Scenes/Assets/Mobs/blue_demon.tscn")
 @onready var redDemon = preload("res://Scenes/Assets/Mobs/red_demon.tscn")
 
@@ -10,3 +18,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_spawnbotblue_pressed():
+	#add_child(blueDemon)
+	# cos nie działa :)
+	pass # Replace with function body.
