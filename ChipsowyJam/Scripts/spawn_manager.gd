568e943693d632be0b9e5d3b42cd7fb50e15ff61
Michał Kornaus <michal990909@wp.pl>
2024-03-28 12:51:26 +0100

    Spawning mobs

--- ChipsowyJam/Scripts/spawn_manager.gd
+++ ChipsowyJam/Scripts/spawn_manager.gd
@@ -7,8 +7,8 @@ extends Node3D
 @export var markerMidBlue: Marker3D
 @export var markerBotBlue: Marker3D
 
-@onready var blueDemon = preload("res://Scenes/Assets/Mobs/blue_demon.tscn")
-@onready var redDemon = preload("res://Scenes/Assets/Mobs/red_demon.tscn")
+@export var blueDemon : PackedScene
+@export var redDemon : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,8 +19,28 @@ func _ready():
 func _process(delta):
 	pass
 
+func _spawn_bot(color: String, marker: Marker3D):
+	var bot: CharacterBody3D
+	if color == "red":
+		bot = redDemon.instantiate()
+	elif color == "blue":
+		bot = blueDemon.instantiate()
 
+	bot.position = marker.global_position
+	bot.rotation = marker.global_rotation
+	
+	add_child(bot)
+	print(bot.position)
+	
 func _on_spawnbotblue_pressed():
-	#add_child(blueDemon)
-	# cos nie działa :)
-	pass # Replace with function body.
+	_spawn_bot("blue", markerBotBlue)
+func _on_spawnmidblue_pressed():
+	_spawn_bot("blue", markerMidBlue)
+func _on_spawntopblue_pressed():
+	_spawn_bot("blue", markerTopBlue)
+func _on_spawnbotred_pressed():
+	_spawn_bot("red", markerBotRed)
+func _on_spawnmidred_pressed():
+	_spawn_bot("red", markerMidRed)
+func _on_spawntopred_pressed():
+	_spawn_bot("red", markerTopRed)
