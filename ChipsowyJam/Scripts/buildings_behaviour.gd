8796b1d3f575c1807f4f1c4ed282f4464984d49b
Macherson <162764064+Macherson@users.noreply.github.com>
2024-04-06 17:50:20 +0200

    Merge

--- ChipsowyJam/Scripts/buildings_behaviour.gd
+++ ChipsowyJam/Scripts/buildings_behaviour.gd
@@ -17,6 +17,7 @@ func _ready():
 		teamName = "red"
 	elif is_in_group("blue_team"):
 		teamName = "blue"
+	
 	if is_in_group("nexus"):
 		type = 2
 	elif is_in_group("tower"):
@@ -34,3 +35,44 @@ func take_damage(amount, attacker):
 func _process(delta):
 	pass
 
+func closest_target():
+	var body: Node3D
+	var dist: float = INF
+	for opponent in opponents_array:
+		var new_dist = self.position.distance_to(opponent.position)
+		if new_dist < dist:
+			dist = new_dist
+			body = opponent
+	opponent_to_attack = body
+	return body
+
+func _on_detection_area_body_entered(body):
+	if opponents_array.size() == 0:
+		if teamName == "blue":
+			if body.is_in_group("red_team"):
+				opponents_array.append(body)
+				set_target()
+		elif teamName == "red":
+			if body.is_in_group("blue_team"):
+				opponents_array.append(body)
+				set_target()
+	else:
+		if teamName == "blue":
+			if body.is_in_group("red_team"):
+				if opponents_array.find(body) == -1:
+					opponents_array.append(body)
+		elif teamName == "red":
+			if body.is_in_group("blue_team"):
+				if opponents_array.find(body) == -1:
+					opponents_array.append(body)
+
+func _on_detection_area_body_exited(body):
+	1`if opponents_array.size() > 0:
+		opponents_array.erase(body)
+		set_target()
+
+func set_target():
+	if opponents_array.size() == 0:
+		opponent_to_attack = null
+	else:
+		opponent_to_attack = closest_target()
