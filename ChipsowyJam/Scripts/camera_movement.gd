e7676b58a5575917c33d88e805e576a9bcface2b
Michał Kornaus <michal990909@wp.pl>
2024-04-09 00:21:48 +0200

    Mouse interaction with 3D world

--- ChipsowyJam/Scripts/camera_movement.gd
+++ ChipsowyJam/Scripts/camera_movement.gd
@@ -2,9 +2,6 @@ extends Node3D
 
 @export var movement_speed = 25
 @export var camera: Camera3D
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
