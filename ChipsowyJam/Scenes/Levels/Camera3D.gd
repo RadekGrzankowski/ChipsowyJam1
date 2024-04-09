b34337beb0ca0cded895e4e6945c92f57c0ae7a6
Michał Kornaus <michal990909@wp.pl>
2024-04-09 13:45:40 +0200

    UI mouse clicking fix

--- ChipsowyJam/Scenes/Levels/Camera3D.gd
+++ ChipsowyJam/Scenes/Levels/Camera3D.gd
@@ -4,13 +4,34 @@ extends Camera3D
 @onready var mid_tower_ui: Control = get_node("/root/GameNode/HUD/GameUIManager/HUD/UpgradeControls/MidTowerUpgrades")
 @onready var bot_tower_ui: Control = get_node("/root/GameNode/HUD/GameUIManager/HUD/UpgradeControls/BotTowerUpgrades")
 var mouse: Vector2
+var mouse_on_UI: bool = false
+
+func _ready():
+	top_tower_ui.mouse_entered.connect(self._mouse_entered)
+	top_tower_ui.mouse_exited.connect(self._mouse_exited)
+	
+	mid_tower_ui.mouse_entered.connect(self._mouse_entered)
+	mid_tower_ui.mouse_exited.connect(self._mouse_exited)
+	
+	bot_tower_ui.mouse_entered.connect(self._mouse_entered)
+	bot_tower_ui.mouse_exited.connect(self._mouse_exited)
+	
+	var buttons = get_tree().get_nodes_in_group("button")
+	for button in buttons:
+		button.mouse_entered.connect(self._mouse_entered)
+		button.mouse_exited.connect(self._mouse_exited)
 
 func _input(event):
 	if event is InputEventMouse:
 		mouse = event.position
 	if event is InputEventMouseButton and event.is_pressed():
-		if event.button_index == MOUSE_BUTTON_LEFT:
+		if event.button_index == MOUSE_BUTTON_LEFT && !mouse_on_UI:
 			select_node()
+			
+func _mouse_entered():
+	mouse_on_UI = true
+func _mouse_exited():
+	mouse_on_UI = false
 
 func select_node():
 	var worldspace = get_world_3d().direct_space_state
@@ -18,7 +39,7 @@ func select_node():
 	var end = project_position(mouse, 1000)
 	var result = worldspace.intersect_ray(PhysicsRayQueryParameters3D.create(start, end))
 	var collider = result.collider
-	print(result)
+	#print(result)
 	if collider.is_in_group("top_tower"):
 		hide_every_ui()
 		top_tower_ui.visible = true
